506f8d1a407a5a74e039dd6bd47a4254b733cce6
Gabriel Augusto <gabrielgst56@hotmail.com>
2018-11-20 16:50:16 -0200

    inicio do desenvolvimento no frontend

--- markset-frontend/src/app/app.component.ts
+++ markset-frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'markset-frontend';
+export class AppComponent implements OnInit {
+  
+  title = 'Marsket';
+
+  constructor(private router:Router){
+
+  }
+
+
+  ngOnInit() {
+    this.router.navigate(['home']);
+  }
+
 }

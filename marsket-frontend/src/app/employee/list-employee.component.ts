6a3de8460ec2ede7a76ed90dec0b6659f2bace82
PedroLeme <pedro_leme98@hotmail.com>
2018-11-29 19:48:08 -0200

    Up

--- marsket-frontend/src/app/employee/list-employee.component.ts
+++ marsket-frontend/src/app/employee/list-employee.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { EmployeeService } from "../../services/employee.service";
-import { Employee } from "../../models/employee";
+import { EmployeeService } from "../services/employee.service";
+import { Employee } from "../models/employee";
 
 @Component({
     selector: 'list-employee',
